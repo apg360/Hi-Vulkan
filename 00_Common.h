9a9577d967633fdaacfbeae2204e25e4e514e76d
apg360 <53176930+apg360@users.noreply.github.com>
2021-04-20 16:45:56 +0200

    Update 00_Common.h

--- 00_Common.h
+++ 00_Common.h
@@ -53,13 +53,3 @@
     dlg_asserttm(("tag3"), 3 == 2, "The same goes for asserts");
 */
 
-struct VulkanKore {
-    int		width               	= 800;
-    int		height              	= 600;
-	GLFWwindow* windowHandle 		= NULL;
-	VkInstance instance 			= VK_NULL_HANDLE;
-	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-	VkSurfaceKHR surface 			= VK_NULL_HANDLE;
-	VkDevice device 				= VK_NULL_HANDLE;
-	VezSwapchain swapchain 			= VK_NULL_HANDLE;
-};
