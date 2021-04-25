d692c3908fcd43123bd75870cf479d922b36ca07
apg360 <53176930+apg360@users.noreply.github.com>
2021-04-25 16:34:10 +0200

    Update 02_VulkanInstance.h

--- 02_VulkanInstance.h
+++ 02_VulkanInstance.h
@@ -8,8 +8,8 @@
 //Variables and Functions before main function of this step
 VkResult vk_error;
 struct global_parameters * local_VulkanKore_param;
-VezApplicationInfo		appInfo 			= {};
-VezInstanceCreateInfo	instanceCreateInfo 	= {};
+VezApplicationInfo      appInfo             = {};
+VezInstanceCreateInfo   instanceCreateInfo  = {};
 
 //________//________// END
 
@@ -18,38 +18,36 @@ int SetupVulkanLayers();
 
 int SetupVulkanInstance(struct global_parameters *pVulkanKore_param)
 {
-	local_VulkanKore_param=pVulkanKore_param;
-	
-    appInfo.pApplicationName 					= "VulkanKore";
-    appInfo.applicationVersion 					= VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName 						= "VulkanKore";
-    appInfo.engineVersion 						= VK_MAKE_VERSION(1, 0, 0);
-	
-	instanceCreateInfo.pApplicationInfo         = &appInfo;
-	
-	vk_error = vezCreateInstance(&instanceCreateInfo, &local_VulkanKore_param->instance);
-	if (vk_error != VK_SUCCESS) return EXIT_FAILURE;
+    local_VulkanKore_param=pVulkanKore_param;
     
+    appInfo.pApplicationName                    = "VulkanKore";
+    appInfo.applicationVersion                  = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName                         = "VulkanKore";
+    appInfo.engineVersion                       = VK_MAKE_VERSION(1, 0, 0);
+    
+    instanceCreateInfo.pApplicationInfo         = &appInfo;
     vk_error = SetupVulkanExtensions();
     if (vk_error != EXIT_SUCCESS) return EXIT_FAILURE;
-    
     vk_error = SetupVulkanLayers();
     if (vk_error != EXIT_SUCCESS) return EXIT_FAILURE;
     
-	vk_error = glfwCreateWindowSurface(local_VulkanKore_param->instance, local_VulkanKore_param->windowHandle, NULL, &local_VulkanKore_param->surface);
-	if (vk_error != EXIT_SUCCESS) return EXIT_FAILURE;
-	
+    vk_error = vezCreateInstance(&instanceCreateInfo, &local_VulkanKore_param->instance);
+    if (vk_error != VK_SUCCESS) return EXIT_FAILURE;
+    
+    vk_error = glfwCreateWindowSurface(local_VulkanKore_param->instance, local_VulkanKore_param->windowHandle, NULL, &local_VulkanKore_param->surface);
+    if (vk_error != EXIT_SUCCESS) return EXIT_FAILURE;
+    
     //--//--//--//
-	//Cleanup (for every "malloc" there must be a "free"
+    //Cleanup (for every "malloc" there must be a "free"
     
     return EXIT_SUCCESS;
 }// END SetupVulkanInstance()
 
 int SetupVulkanExtensions() {
-	//Initialize or reset value
-	local_VulkanKore_param->enabled_extension_count = 0;
-	uint32_t required_extension_count = 0;
-	// Automatically check and list the required extensions for this Vulkan instance to start
+    //Initialize or reset value
+    local_VulkanKore_param->enabled_extension_count = 0;
+    uint32_t required_extension_count = 0;
+    // Automatically check and list the required extensions for this Vulkan instance to start
     const char **required_extensions = glfwGetRequiredInstanceExtensions(&required_extension_count);
     
     // Build extensions list
@@ -69,14 +67,14 @@ int SetupVulkanExtensions() {
     instanceCreateInfo.enabledExtensionCount           = COUNT_ARRAY_ELEMS(local_VulkanKore_param->enabledExtensions);
     instanceCreateInfo.ppEnabledExtensionNames         = local_VulkanKore_param->enabledExtensions;
     
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 int SetupVulkanLayers() {
-	//
-	local_VulkanKore_param->enabled_layer_count = 0;
-	local_VulkanKore_param->enabledLayers[0] = "VK_LAYER_LUNARG_standard_validation"; // VK_LAYER_KHRONOS_validation or VK_LAYER_LUNARG_standard_validation or "VK_LAYER_NV_optimus"
-	local_VulkanKore_param->enabled_layer_count = 1; //COUNT_ARRAY_ELEMS(local_VulkanKore_param->enabledLayers);
+    //
+    local_VulkanKore_param->enabled_layer_count = 0;
+    local_VulkanKore_param->enabledLayers[0] = "VK_LAYER_LUNARG_standard_validation"; // VK_LAYER_KHRONOS_validation or VK_LAYER_LUNARG_standard_validation or "VK_LAYER_NV_optimus"
+    local_VulkanKore_param->enabled_layer_count = 1; //COUNT_ARRAY_ELEMS(local_VulkanKore_param->enabledLayers);
     
     //List the enabled Layers
     dlg_info("Enabled layers count %u",local_VulkanKore_param->enabled_layer_count);
@@ -88,5 +86,5 @@ int SetupVulkanLayers() {
     //
     instanceCreateInfo.ppEnabledLayerNames = local_VulkanKore_param->enabledLayers;
     
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
